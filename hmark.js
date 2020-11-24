6ca14f106c6a807d1c262f670db9f2c35c93b2da
Manu Cornet <m@ma.nu>
2020-11-24 16:24:27 +0000

    Draw data on a chart

--- hmark.js
+++ hmark.js
@@ -1,3 +1,5 @@
+const CHARTJS = 'https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js';
+
 /** The paths (relative URLs) to test pages. */
 let PATHS = [];
 
@@ -72,25 +74,91 @@ function renderSingleSeriesStats(timings) {
   const squaredDiffsSum = squaredDiffs.reduce((a, b) => a + b, 0);
   const stdDev = Math.sqrt(squaredDiffsSum / timings.length);
 
+  console.log(bellCurvify(timings));
+
   const rendered = '<big>Rendered ' + timings.length + ' times. ' +
       'Average render time: <b>' + average.toFixed(1) + ' ms.</b> ' +
       'Standard deviation: <b>' + stdDev.toFixed(1) + ' ms.</b></big>';
   return rendered;
 }
 
+function bellCurvify(series, min, max) {
+  // Construct an object to track how many times a given timing was measured.
+  let bell = {};
+  for (let i = min; i <= max; i++) {
+    bell[i] = 0;
+  }
+  for (let i = 0; i < series.length; i++) {
+    bell[series[i]] += 1;
+  }
+
+  let outSeries = [];
+  // Now make this into an array.
+  for (let i = min; i <= max; i++) {
+    outSeries.push(bell[i]);
+  }
+  return outSeries;
+}
+
+function getFileName(path) {
+  let filename = path;
+  let dir = filename.indexOf('/');
+  if (dir != -1) {
+    filename = filename.substring(dir + 1);
+  }
+  return filename;
+}
+
 /** Called when we are done and want to show the timings. */
 function finish() {
+  let chartjs = document.createElement('script');
+  chartjs.setAttribute('src', CHARTJS);
+  chartjs.onload = drawChart;
+  document.body.appendChild(chartjs);
   let contents = '';
   for (let i = 0; i < PATHS.length; i++) {
-    let filename = PATHS[i];
-    let dir = filename.indexOf('/');
-    if (dir != -1) {
-      filename = filename.substring(dir + 1);
-    }
-    contents += '<h1>' + filename + '</h1>';
+    contents += '<h1>' + getFileName(PATHS[i]) + '</h1>';
     contents += renderSingleSeriesStats(TIMINGS[i]);
   }
-  document.body.innerHTML = contents;
+  const contentsEl = document.createElement('div');
+  contentsEl.innerHTML = contents;
+  document.body.appendChild(contentsEl);
+}
+
+function drawChart() {
+  const canvas = document.createElement('canvas');
+  let data = [];
+  let min = Infinity;
+  let max = 0;
+  for (let i = 0; i < TIMINGS.length; i++) {
+    let series = TIMINGS[i];
+    min = Math.min(min, Math.min(...series));
+    max = Math.max(max, Math.max(...series));
+  }
+
+  for (let i = 0; i < TIMINGS.length; i++) {
+    let series = TIMINGS[i];
+    data.push({
+      data: bellCurvify(series, min, max),
+      label: getFileName(PATHS[i]),
+      fill: false
+    });
+  }
+  const context = canvas.getContext('2d');
+  let labels = [];
+  for (let i = min; i <= max; i++) {
+    labels.push('' + i);
+  }
+  document.body.appendChild(canvas);
+  console.log(data);
+  new Chart(context, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: data
+    },
+    options: {}
+  });
 }
 
 /** Clears the page and moves on to the next render. */
